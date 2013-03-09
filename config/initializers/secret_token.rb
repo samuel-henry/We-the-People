7547b53d1a2e854c80da7fdcbd3878e5f0ced201
Samuel Henry <samuelhenry@gmail.com>
2013-03-09 14:06:53 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WeThePeople::Application.config.secret_token = 'e3cc8cf8225603700e8fde7677ee4dd0b571380b42b0285b1fe89e82415d99766a8fef698e8d0cb702e468b0cf8d3d4582e8d498fd2e13f027b4ab4f3e4cc772'
