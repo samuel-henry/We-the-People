7aa6a7636ce60489335d6072119893f2ce01e370
Samuel Henry <samuelhenry@gmail.com>
2013-03-10 22:36:10 -0500

    added db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+hello_speech = Speech.create
+hello_speech.date = "3/10/13"
+hello_speech.location = "Chicago, IL"
+hello_speech.speaker = "Samuel Henry"
+hello_speech.title = "Hello World"
+hello_speech.text = ["Hello","World","to","Samuel","Henry"]
+hello_speech.save
+
+
+ray_speech = Speech.create
+ray_speech.date = "12/12/99"
+ray_speech.location = "Pittsburgh, PA"
+ray_speech.speaker = "Ray Lewis"
+ray_speech.title = "Pre-Game Speech"
+ray_speech.text = ["We","will","totally","win"]
+ray_speech.save
+
+
