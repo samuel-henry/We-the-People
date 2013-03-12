d6a9bfe10836260a4b11a3be865d4416576e5889
Samuel Henry <samuelhenry@gmail.com>
2013-03-12 17:30:07 -0500

    change speech text from string to longtext

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130309202101) do
+ActiveRecord::Schema.define(:version => 20130312222916) do
 
   create_table "speeches", :force => true do |t|
     t.string   "date"
     t.string   "location"
     t.string   "speaker"
-    t.string   "text", :limit => 4294967295
+    t.string   "text"
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
