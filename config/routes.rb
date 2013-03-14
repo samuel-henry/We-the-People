b98b6bcfc7ded0c98498d67ba359ac571427b280
Samuel Henry <samuelhenry@gmail.com>
2013-03-14 02:17:08 -0500

    styling + adding more speeches

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ WeThePeople::Application.routes.draw do
 	root :to => 'speeches#index'
 	get '/about' => 'about#index'
 
-	get     '/:speeches'        => 'speeches#index'
-  	get     '/:speeches/new'    => 'speeches#index'
-  	post    '/speeches'         => 'speeches#index'
-  	get     '/speeches/:id'     => 'speeches#show'
-  	get     'speeches/:id/edit' => 'speeches#index'
-  	put     '/speeches/:id'     => 'speeches#index'
-  	delete  '/speeches/:id'     => 'speeches#index'
-  	resources :speeches
+	#get     '/:speeches'        => 'speeches#index'
+	#get     '/:speeches/new'    => 'speeches#index'
+	#post    '/speeches'         => 'speeches#index'
+	#get     '/speeches/:id'     => 'speeches#show'
+	#get     'speeches/:id/edit' => 'speeches#index'
+	#put     '/speeches/:id'     => 'speeches#index'
+	#delete  '/speeches/:id'     => 'speeches#index'
+	resources :speeches
 
 end
