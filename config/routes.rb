f69e60c95d2b96c253505902cb599669550fb774
Sam Henry <samuelhenry@gmail.com>
2013-07-09 16:52:31 -0700

    temporarily allow deletion of speeches

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ WeThePeople::Application.routes.draw do
 	get     '/:speeches/new'    => 'speeches#index'
 	post    '/speeches'         => 'speeches#index'
 	get     '/speeches/:id'     => 'speeches#show'
-	get     'speeches/:id/edit' => 'speeches#index'
-	put     '/speeches/:id'     => 'speeches#index'
-	delete  '/speeches/:id'     => 'speeches#index'
+	#get     'speeches/:id/edit' => 'speeches#index'
+	#put     '/speeches/:id'     => 'speeches#index'
+	#delete  '/speeches/:id'     => 'speeches#index'
 	resources :speeches
 
 end
